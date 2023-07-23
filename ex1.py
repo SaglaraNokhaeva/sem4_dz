21baff9bdc5cf8266ca67b5ec7487c3ea53a6ef6
Saglara <nohaevass@yandex.ru>
2023-07-23 12:00:27 +0300

    1

--- ex1.py
+++ ex1.py
@@ -1,4 +1,17 @@
 # Напишите функцию для транспонирования матрицы
 
+column, line = input("Введите количество столбцов и строк через пробел: ").split()
 matrix = input("Введите элементы матрицы через пробел: ").split()
+# print(column, line)
+# print(type(column))
+# print(type(line))
 
+
+for i in range(len(matrix)):
+    _ = 0
+    for j in range((i + _), int(column)):
+        print(matrix[j])
+    _ += int(column)
+    print("\n")
+
+# def transon_matrix(matrix, column, line)
