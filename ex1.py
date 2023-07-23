2636c5989101fc9fa2e01c939f089c56a5d4129c
Saglara <nohaevass@yandex.ru>
2023-07-23 14:10:09 +0300

    1

--- ex1.py
+++ ex1.py
@@ -2,19 +2,20 @@
 
 column, line = input("Введите количество столбцов и строк через пробел: ").split()
 matrix = input("Введите элементы матрицы через пробел: ").split()
-# print(column, line)
-# print(type(column))
-# print(type(line))
+
+print("Исходная матрица: ")
+for i in range(0, len(matrix), int(column)):
+    for j in range(i, int(column)+i):
+        print(matrix[j], " ", end="")
+    print()
+
+print("Транспонированная матрица: ")
+def transon_matrix(matrix, column, line):
+    for i in range(0, int(column)):
+        for j in range(i, len(matrix), int(column)):
+            print(matrix[j], " ", end="")
+        print()
+
+transon_matrix(matrix, column, line)
 
 
-for i in range(0, len(matrix), 2):
-    # print(i)
-    # print(len(matrix), int(column))
-    # _ = 0
-    for j in range(i, int(column)):
-        # print(j, int(column))
-        print(matrix[j], end="")
-    # _ += int(column)
-    print("\n")
-
-# def transon_matrix(matrix, column, line)
